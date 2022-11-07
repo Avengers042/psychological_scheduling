b41db0874b93538933ee80bf265243e54eea1705
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-11-07 16:44:56 -0300

    :feat: add @transaction save to persist data

--- src/main/java/com/avengers/psychological_scheduling/services/StudentService.java
+++ src/main/java/com/avengers/psychological_scheduling/services/StudentService.java
@@ -1,6 +1,9 @@
 package com.avengers.psychological_scheduling.services;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +21,11 @@ public class StudentService {
     this.studentRepository = studentRepository;
   }
 
+  @Transactional
+  public StudentModel save(StudentModel studentModel) {
+    return studentRepository.save(studentModel);
+  }
+
   public Page<StudentModel> findAllStudent(Pageable pageable) {
     return studentRepository.findAll(pageable);
   }
@@ -25,4 +33,9 @@ public class StudentService {
   public List<StudentModel> findStudentByName(String name) {
     return studentRepository.findStudentByNameLike(name);
   }
+
+  public Optional<StudentModel> findByRegistration(Long registration) {
+    return studentRepository.findById(registration);
+  }
+
 }
