f5abd9d8af08a67fa55a82628c198f0b27ac33ed
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-11-07 16:31:37 -0300

    :sparkles: add CRU of student This commits include many modifications from @Matheus-Mapg, because we talked about padronize routes. Moreover, change from rendering List as HTML tags to body json. In situation of search user by registration and search like (LIKE from SQL), change the routes, because there was some conflicts in my previous thinking. There is some style changes and bugs fixed in this code."

--- src/main/java/com/avengers/psychological_scheduling/controllers/StudentController.java
+++ src/main/java/com/avengers/psychological_scheduling/controllers/StudentController.java
@@ -1,22 +1,32 @@
 package com.avengers.psychological_scheduling.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.avengers.psychological_scheduling.dtos.users.StudentDto;
 import com.avengers.psychological_scheduling.models.users.StudentModel;
 import com.avengers.psychological_scheduling.services.StudentService;
 
 @RestController
 @RequestMapping("/students")
-public class StudentController implements UserController<StudentModel> {
+public class StudentController {
 
   final StudentService studentService;
 
@@ -24,41 +34,51 @@ public class StudentController implements UserController<StudentModel> {
     this.studentService = studentService;
   }
 
-  @Override
-  public ResponseEntity<Object> addUserToSystem(StudentModel user) {
-    return null;
+  @PostMapping
+  public ResponseEntity<Object> addUserToSystem(@RequestBody @Valid StudentDto studentDto) {
+    StudentModel studentModel = new StudentModel();
+    BeanUtils.copyProperties(studentDto, studentModel);
+    studentModel.setActivationStatus(true);
+    studentModel.setJob(1);
+    return ResponseEntity.status(HttpStatus.CREATED).body(studentService.save(studentModel));
   }
 
   @GetMapping
-  public Page<StudentModel> searchAllUser(Pageable pageable) {
-
-    return studentService.findAllStudent(pageable);
+  public ResponseEntity<Page<StudentModel>> searchAllUser(
+      @PageableDefault(page = 0, size = 10, sort = "registration", direction = Sort.Direction.ASC) Pageable pageable) {
+    return ResponseEntity.status(HttpStatus.OK).body(studentService.findAllStudent(pageable));
   }
 
-  @GetMapping("/searchUser/{name}")
+  @GetMapping("/search-user/name/{name}")
   public ResponseEntity<Object> searchUserByName(@PathVariable("name") String name) {
-
     List<StudentModel> listStudent = studentService.findStudentByName("%" + name + "%");
 
-    String listStudentString = "";
+    if (listStudent.isEmpty())
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found.");
 
-    for (StudentModel student : listStudent) {
-
-      listStudentString += "Nome: " + student.getName() + "<br>";
-      listStudentString += "Nome: " + student.getEmail() + "<br><br>";
-    }
-
-    return ResponseEntity.status(HttpStatus.OK).body(listStudentString);
+    return ResponseEntity.status(HttpStatus.OK).body(listStudent);
   }
 
-  @Override
-  public ResponseEntity<Object> searchAllUserByRegistration(int registration) {
-    return null;
+  @GetMapping("/search-user/id/{registration}")
+  public ResponseEntity<Object> searchUserByRegistration(@PathVariable(value = "registration") Long registration) {
+    Optional<StudentModel> studentModelOptional = studentService.findByRegistration(registration);
+    if (!studentModelOptional.isPresent())
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found.");
+
+    return ResponseEntity.status(HttpStatus.OK).body(studentModelOptional.get());
   }
 
-  @Override
-  public ResponseEntity<Object> updateDataOfUser(int registration, StudentModel user) {
-    return null;
+  @PutMapping("/{registration}")
+  public ResponseEntity<Object> updateDataOfUser(@PathVariable(value = "registration") Long registration,
+      @RequestBody @Valid StudentDto studentDto) {
+    Optional<StudentModel> studentModelOptional = studentService.findByRegistration(registration);
+    if (!studentModelOptional.isPresent())
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Student not found.");
+
+    StudentModel studentModel = new StudentModel();
+    BeanUtils.copyProperties(studentDto, studentModel);
+    studentModel.setRegistration(studentModelOptional.get().getRegistration());
+    return ResponseEntity.status(HttpStatus.OK).body(studentService.save(studentModel));
   }
 
 }
